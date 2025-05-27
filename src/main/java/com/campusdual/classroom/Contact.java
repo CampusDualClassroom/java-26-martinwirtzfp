91a86439be9d9e16ff666f09ee895998501510bc
Martin Sanchez Novo <martinwirtzfp@gmail.com>
2025-05-27 13:01:59 +0200

    Ejercicio 26 completado y testeado

--- src/main/java/com/campusdual/classroom/Contact.java
+++ src/main/java/com/campusdual/classroom/Contact.java
@@ -1,5 +1,104 @@
 package com.campusdual.classroom;
 
-public class Contact{
+import java.text.Normalizer;
+import java.util.regex.Pattern;
 
+public class Contact implements ICallActions{
+    private String name;
+    private String surname1;
+    private String phone;
+    private String code;
+
+    public Contact(String name, String surname1, String phone){
+        this.name = name;
+        this.surname1 = surname1;
+        this.phone = phone;
+        this.code = contactCodeGenerator();
+    }
+
+    private String contactCodeGenerator(){
+        //Primero tratamos el nombre
+        char nombre = this.name.toLowerCase().charAt(0);
+        //Le pasamos el apellido a la funcion charCount para que nos pase el numero de espacios
+        int spaces = charCount(this.surname1, ' ');
+        String apellido;
+        //Actuamos en función del número de espacios
+        if(spaces == 0){
+            apellido = normalize(this.surname1);
+        } else {
+            int ruptura = 0;
+            ruptura = this.surname1.indexOf(' ');
+            apellido = this.surname1.substring(0,1);
+            apellido += this.surname1.substring(ruptura+1);
+            //Lo normalizamos
+            apellido = normalize(apellido);
+            //Eliminamos sus espacios
+            apellido = removeSpaces(apellido);
+        }
+        return nombre+apellido;
+    }
+
+    private static int charCount(String string, char character) {
+        int contador = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == character) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    private static String normalize(String texto) {
+        // Normaliza el texto (por ejemplo: "á" → "á")
+        String textoNormalizado = Normalizer.normalize(texto, Normalizer.Form.NFD);
+        // Elimina los caracteres que no son ASCII (los diacríticos)
+        Pattern patron = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+        return (patron.matcher(textoNormalizado).replaceAll("")).toLowerCase();
+    }
+
+    private static String removeSpaces (String string){
+        StringBuilder sb = new StringBuilder(string);
+
+        for (int i = 0; i < sb.length(); i++) {
+            if (Character.isWhitespace(sb.charAt(i))) {
+                sb.deleteCharAt(i);
+                i--; // Retrocede una posición porque eliminaste un carácter
+            }
+        }
+        return sb.toString();
+    }
+
+
+    @Override
+    public void callMyNumber() {
+        System.out.println("El contacto " + this.name + " " + this.surname1 +
+                " con numero " + this.phone + " se está llamando a si mismo");
+    }
+
+    @Override
+    public void callOtherNumber(String number) {
+        System.out.println("El contacto " + this.name + " " + this.surname1 + " está llamando a " + number);
+    }
+
+    @Override
+    public void showContactDetails() {
+        System.out.println("Name: " + this.name + " Surname: " + this.surname1 + " Phone: " + this.phone
+        + " Code: " + this.code);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurnames() {
+        return surname1;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getCode() {
+        return code;
+    }
 }
--- src/main/java/com/campusdual/classroom/Phonebook.java
+++ src/main/java/com/campusdual/classroom/Phonebook.java
@@ -1,5 +1,141 @@
 package com.campusdual.classroom;
 
+import com.campusdual.util.Utils;
+import jdk.jshell.execution.Util;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.TreeMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class Phonebook {
+    private Map<String, Contact> agenda;
+
+    public Phonebook() {
+        this.agenda = new TreeMap<>();
+    }
+
+    public Map<String, Contact> getData() {
+        return agenda;
+    }
+
+    //p.showPhonebook()
+    public void addContact(Contact c){
+        //Clave del contacto
+        String clave = c.getCode();
+        //Añadimos el contacto
+        this.agenda.put(clave, c);
+    };
+
+    public void showPhonebook(){
+        for (Map.Entry<String, Contact> entry: agenda.entrySet()){
+            entry.getValue().showContactDetails();
+        }
+    }
+
+    public boolean deleteContact(String c){ //Recibe como parámetro el código de un contacto
+        //Primero comprobamos si el contacto existe
+        boolean match = false;
+        for(Map.Entry<String, Contact> entry: this.agenda.entrySet()){
+            if (Objects.equals(entry.getKey(), c)){
+                match = true;
+                break;
+            }
+        }
+
+        //Si existe lo borramos
+        if (match){
+            this.agenda.remove(c);
+        }
+        return match;
+    }
+
+    public Contact buscarContacto(String c){
+        Contact contact = null;
+        for(Map.Entry<String, Contact> entry: this.agenda.entrySet()){
+            if (Objects.equals(entry.getKey(), c)){
+                contact = entry.getValue();
+                break;
+            }
+        }
+        return contact;
+    }
+
+    public static void ejecutable(Phonebook p){
+        int option = 0;
+        do{
+            System.out.println("Selecciona una opcion: ");
+            System.out.println("1. Añadir contacto");
+            System.out.println("2. Mostrar contactos");
+            System.out.println("3. Opciones de contacto");
+            System.out.println("4. Eliminar un contacto");
+            System.out.println("5.  Salir");
+            option = Utils.integer();
+            switch (option){
+                case 1:{
+                    String nombre;
+                    String apellido;
+                    String numero;
+                    System.out.println();
+                    nombre = Utils.string("Introduce el nombre");
+                    apellido = Utils.string("Introduce el apllido o apellidos");
+                    numero = Utils.string("Introduce el numero de telefono");
+                    Contact contact = new Contact(nombre, apellido, numero);
+
+                    p.addContact(contact);
+
+                    System.out.println("Contacto añadido correctamente");
+                    break;
+                }
+                case 2:{
+                    System.out.println("Lista de contactos");
+                    p.showPhonebook();
+                    break;
+                }
+                case 3:{
+                    String codigo;
+                    codigo = Utils.string("Introduce un codigo de contacto");
+                    Contact contact = p.buscarContacto(codigo);
+                    int contactOption = 0;
+                    System.out.println("Selecciona una opcion");
+                    System.out.println("1. Call my number");
+                    System.out.println("2. Call other number");
+                    System.out.println("3. Show contact details");
+                    contactOption = Utils.integer();
+                    switch (contactOption){
+                        case 1:{
+                            contact.callMyNumber();
+                        }
+                        case 2:{
+                            String codtocall;
+                            codtocall = Utils.string("Introduce el numero del contacto para llamarlo");
+                            contact.callOtherNumber(codtocall);
+                        }
+                        case 3:{
+                            contact.showContactDetails();
+                        }
+                    }
+                    break;
+
+                }
+                case 4:{
+                    String cod;
+                    cod = Utils.string("Introduce el codigo del contacto a borrar: ");
+                    if (p.deleteContact(cod)){
+                        System.out.println("Contatco eliminado con exito");
+                    } else {
+                        System.out.println("El contacto introducido no existe");
+                    }
+                }
+                case 5:{
+                    System.out.println("Finalizando el programa...");
+                }
+            }
+        } while (option != 5);
+    }
+
+
 
 }
